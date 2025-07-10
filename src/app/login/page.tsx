13c1f70161abc50754b249e6604ef3367e6c4a7d
Ritik Jain <ritikjain@Ritiks-MacBook-Air.local>
2025-07-10 21:48:37 +0530

    fixed text

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -41,7 +41,7 @@ export default function LoginPage() {
           <Button type="submit" className="w-full">Login</Button>
         </form>
         <div className="mt-4 text-center text-sm">
-          Don't have an account? <Link href="/signup" className="text-blue-600 hover:underline">Sign up</Link>
+          Do not have an account? <Link href="/signup" className="text-blue-600 hover:underline">Sign up</Link>
         </div>
       </Card>
     </div>
